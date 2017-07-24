f1bed3a0cffb77eadf27ff84a38375306b5385a8
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-07-24 09:30:16 +0200

    simple layout

--- js/permascript.js
+++ js/permascript.js
@@ -1,2 +1,39 @@
 ---
 ---
+
+class PermaScript {
+  constructor(canonName) {
+    this.canonName = canonName;
+  }
+
+  setup() {
+    $("#load-progress").show(500);
+    let canonUrl = `/canon/${this.canonName}.txt`;
+    $.get(canonUrl, this.receiveCanon.bind(this), "text");
+  }
+
+  receiveCanon(text, status, jqxhr) {
+    $("#load-progress").hide(500);
+    this.setCanon(text);
+  }
+
+  setCanon(text) {
+    this.canon = text.split("\n").map(this.normalizeLine).filter(this.nonEmptyLine);
+    let container = $("#permascript");
+    let lineNos = $('<div class="line-nos">').appendTo(container);
+    let lines = $('<div class="lines">').appendTo(container);
+    for (let [index, line] of this.canon.entries()) {
+      index = index + 1;
+      lineNos.append($(`<div><a href="#${index}" id="${index}">${index}</div>`));
+      lines.append($(`<div id="line-${index}">`).text(line));
+    }
+  };
+
+  normalizeLine(line) {
+    return line.trim();
+  }
+
+  nonEmptyLine(line) {
+    return line.length > 0;
+  }
+}
