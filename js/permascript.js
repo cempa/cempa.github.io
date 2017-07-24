31da62ac9742eb2bc2455cfb9983f6d74c987172
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-07-24 11:15:18 +0200

    highlight permalinked line

--- js/permascript.js
+++ js/permascript.js
@@ -10,6 +10,7 @@ class PermaScript {
     $("#load-progress").show(500);
     let canonUrl = `/canon/${this.canonName}.txt`;
     $.get(canonUrl, this.receiveCanon.bind(this), "text");
+    $(window).on("popstate", this.onPopState.bind(this));
   }
 
   receiveCanon(text, status, jqxhr) {
@@ -25,8 +26,9 @@ class PermaScript {
     for (let [index, line] of this.canon.entries()) {
       index = index + 1;
       lineNos.append($(`<div class="line-no"><a href="#${index}" id="${index}">${index}</div>`));
-      lines.append($(`<div id="line-${index}" class="line">`).text(line));
+      lines.append($(`<div id="line-${index}" class="line"><span class="line-text">${line}</span></div>`));
     }
+    this.setPermahash(window.location.hash);
     window.location = window.location;
   };
 
@@ -36,5 +38,15 @@ class PermaScript {
 
   nonEmptyLine(line) {
     return line.length > 0;
+  }
+
+  setPermahash(hash) {
+    let [address, lineNo] = /^#(\d+)/.exec(hash);
+    $(".line .selected").removeClass("selected");
+    $(`#line-${lineNo}`).addClass("selected");
+  }
+
+  onPopState() {
+    this.setPermahash(window.location.hash);
   }
 }
